312b34551b88431625a8090c39dd03c64f267cc9
Dmitriy Buldakov <dabuldakov@mail.ru>
2019-08-29 17:43:39 +0700

    1

--- Demo.hs
+++ Demo.hs
@@ -131,13 +131,14 @@ length' xs = sum [1 | _ <- xs]
 
 
 
-
+sum'n'count :: Integer -> (Int, Int)
 sum'n'count x | x > 0 = ((dlina x), (summa 0 (show x) ((dlina x)-1)))
-sum'n'count x | x < 0 = (dlina2 x, 0)
+sum'n'count x | x < 0 = (dlina2 x, (summa 0 (tail (show x)) ((dlina2 x)-1)))
 sum'n'count x | x == 0 = (1,0)
 
 dlina x = length (show x)
 dlina2 x = length (tail (show x))
+
 
 summa acc x (-1) = acc
 summa acc x n = summa (digitToInt(x !! n) + acc) x (n-1)
