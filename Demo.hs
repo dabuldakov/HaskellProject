7e19fb037911536e7c0372ebbf9e45a976a71359
dabuldakov <54432971+dabuldakov@users.noreply.github.com>
2019-09-01 18:23:59 +0700

    tipe classes

--- Demo.hs
+++ Demo.hs
@@ -190,3 +190,9 @@ doItYourself = f . g . h
 f = logBase 2
 g = (^3)
 h = max 42
+
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+ deriving (Show)
+area :: Shape -> Float
+area (Circle _ _ r) = pi * r^2
+area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
--- hello.hs
+++ hello.hs
@@ -1,1 +1,9 @@
-main = putStrLn "Hello World"
+class Printable a where
+ (True), (False), (()) :: a -> [Char]
+
+
+instance Printable Bool where
+  toSting x = if x == True then "true" else "false"
+  
+instance Printable () where
+  toSting x = "unit type"
