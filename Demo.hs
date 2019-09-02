dade7119739040e01add7598c5b3a5ec0bdec975
Dmitriy Buldakov <dabuldakov@mail.ru>
2019-09-02 17:25:46 +0700

    qwe

--- Demo.hs
+++ Demo.hs
@@ -202,8 +202,7 @@ data Point = Point Float Float
  
 nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x + a) (y + b)) r
-nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = 
- Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
  
 baseCircle :: Float -> Shape 
 baseCircle r = Circle (Point 0 0) r 
