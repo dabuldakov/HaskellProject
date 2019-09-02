c9549426d9a817ecb2e6d273b70ae3f137bd4a25
dabuldakov <54432971+dabuldakov@users.noreply.github.com>
2019-09-02 22:36:25 +0700

    Update baby.hs

--- baby.hs
+++ baby.hs
@@ -54,16 +54,18 @@ charName  x  = "Another"
 length'' xs = sum[1 | _ <- xs]
 
 spisok :: [Char] -> [Char]
-spisok x = replace x [] (length x) (x !! (length x)) 
+spisok x = replace (reverse x) "" (dlinaX x) ((reverse x) !! (dlinaX x)) 
+
+dlinaX x = (length x) - 1
 
 replace :: [Char] -> [Char] -> Int -> Char -> [Char]
-replace list list2 0 '_' = list2
-replace list list2 n 'A' = replace list (list2 ++ (list !! n)) n (list !! (n-1))
-replace list list2 n 'T' = replace list (list2 ++ (list !! n)) n (list !! (n-1))
-replace list list2 n 'G' = replace list (list2 ++ (list !! n)) n (list !! (n-1))
-replace list list2 n 'C' = replace list (list2 ++ (list !! n)) n (list !! (n-1))
+replace list list2 (-1)  _ = list2
+replace list list2 n 'A' = replace list (list2 ++ "T") (n-1) (list !! (n-1))
+replace list list2 n 'T' = replace list (list2 ++ "A") (n-1) (list !! (n-1))
+replace list list2 n 'G' = replace list (list2 ++ "C") (n-1) (list !! (n-1))
+replace list list2 n 'C' = replace list (list2 ++ "G") (n-1) (list !! (n-1))
 
-spisok' 
+
 
 
 
