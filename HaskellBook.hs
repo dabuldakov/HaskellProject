7823b7693008f33314eb61360c82f1b70e21c53e
dabuldakov <54432971+dabuldakov@users.noreply.github.com>
2019-09-12 22:08:16 +0700

    1

--- HaskellBook.hs
+++ HaskellBook.hs
@@ -158,10 +158,16 @@ quickSort' (x:xs) = quickSort a ++ [x] ++ quickSort b
  where
   a = [y | y <- xs, y <= x]
   b = [y | y <- xs, y >  x]
+  
+---------------------
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
-
-
-
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = q 
+ where q x y = f y x 
 
 
 
