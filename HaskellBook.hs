aaacdc08e2934fbe873da434a86ab48890061848
Dmitriy Buldakov (Job) <dabuldakov@mail.ru>
2019-09-30 16:54:09 +0700

    IO...

--- HaskellBook.hs
+++ HaskellBook.hs
@@ -502,9 +502,19 @@ threeCoins gen =
   (thirdCoin, newGen'') = random newGen'
  in (firstCoin, secondCoin, thirdCoin)
 
+randoms' :: (RandomGen g, Random a) => g -> [a]
+randoms' gen = 
+ let 
+  (value, newGen) = random gen 
+ in value:randoms' newGen
 
-
-
+finiteRandoms :: (RandomGen g, Random a, Num n, Eq n) => n -> g -> ([a], g)
+finiteRandoms 0 gen = ([], gen)
+finiteRandoms n gen =
+  let 
+   (value, newGen) = random gen
+   (restOfList, finalGen) = finiteRandoms (n-1) newGen
+  in (value:restOfList, finalGen)
 
 
 
