55dfc291f1948f136d856bfef43677e7d091010f
dabuldakov <54432971+dabuldakov@users.noreply.github.com>
2019-10-01 21:32:15 +0700

    best path end

--- HaskellBook.hs
+++ HaskellBook.hs
@@ -532,38 +532,6 @@ solveRPN = head . foldl f [] . words
 
 ----------------------------
 
-data Section = Section { getA :: Int,
-                         getB :: Int,
-                         hetC :: Int } deriving (Show)
-
-type RoadSystem = [Section]
-
-heathrowToLondon :: RoadSystem
-heathrowToLondon = [ Section 50 10 30
-                   , Section  5 90 20
-                   , Section 40  2 25
-                   , Section 10  8  0 ]
-
-data Label = A | B | C deriving (Show)
-type Path  = [(Label, Int)]
-
---optimalPath :: RoadSystem -> Path
-
-roadStep :: (Path, Path) -> Section -> (Path, Path)
-roadStep (pathA, pathB) (Section a b c) = 
-  let timeA = sum $ map snd pathA
-      timeB = sum $ map snd pathB
-      forwardTimeToA = timeA + a
-      crossssTimeToA = timeB + b + c
-      forwardTimeToB = timeB + b
-      crossssTimeToB = timeA + a + c
-      newPathToA = if forwardTimeToA <= crossssTimeToA
-                      then       (A,a):pathA
-                      else (C,c):(B,b):pathB
-      newPathToB = if forwardTimeToB <= crossssTimeToB
-                      then       (B,b):pathB
-                      else (C,c):(A,a):pathA
-  in (newPathToA, newPathToB)
 
 
 
---
+++ bestPath.hs
@@ -0,0 +1,68 @@
+﻿import Data.List
+import System.IO
+import System.Directory
+import System.Environment 
+
+
+data Section = Section { getA :: Int,
+                         getB :: Int,
+                         getC :: Int } deriving (Show)
+
+type RoadSystem = [Section]
+
+heathrowToLondon :: RoadSystem
+heathrowToLondon = [ Section 50 10 30
+                   , Section  5 90 20
+                   , Section 40  2 25
+                   , Section 10  8  0 ]
+
+data Label = A | B | C deriving (Show)
+type Path  = [(Label, Int)]
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem = 
+  let (bestApath, bestBpath) = foldl roadStep ([],[]) roadSystem
+  in if sum (map snd bestApath) <= sum (map snd bestBpath)
+        then reverse bestApath
+        else reverse bestBpath
+
+
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _ = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
+
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) = 
+  let timeA = sum $ map snd pathA
+      timeB = sum $ map snd pathB
+      forwardTimeToA = timeA + a
+      crossssTimeToA = timeB + b + c
+      forwardTimeToB = timeB + b
+      crossssTimeToB = timeA + a + c
+      newPathToA = if forwardTimeToA <= crossssTimeToA
+                      then       (A,a):pathA
+                      else (C,c):(B,b):pathB
+      newPathToB = if forwardTimeToB <= crossssTimeToB
+                      then       (B,b):pathB
+                      else (C,c):(A,a):pathA
+  in (newPathToA, newPathToB)
+
+main = do
+  contents <- getContents
+  let threes = groupsOf 3 (map read $ lines contents)
+      roadSystem = map (\[a,b,c] -> Section a b c) threes
+      path = optimalPath roadSystem
+      pathString = concat $ map (show. fst) path
+      pathTime = sum $ map snd path
+  putStrLn $ "Brst path: " ++ pathString
+  putStrLn $ "Time: " ++ show pathTime
+
+
+
+
+
+
+
+
